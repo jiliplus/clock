5f88cd5117ad44684b2470755e651f18c3b30b5c
aQua <6028869+aQuaYi@users.noreply.github.com>
2020-01-25 15:12:47 +0800

    整理 context 文件

--- context.go
+++ context.go
@@ -5,6 +5,30 @@ import (
 	"time"
 )
 
+type mockCtx struct {
+	context.Context
+	deadline time.Time
+	done     chan struct{}
+	err      error
+}
+
+func (ctx *mockCtx) Deadline() (time.Time, bool) {
+	return ctx.deadline, true
+}
+
+func (ctx *mockCtx) Done() <-chan struct{} {
+	return ctx.done
+}
+
+func (ctx *mockCtx) Err() error {
+	select {
+	case <-ctx.done:
+		return ctx.err
+	default:
+		return nil
+	}
+}
+
 type clockKey struct{}
 
 // Set 把 Clock 放入 ctx 中
