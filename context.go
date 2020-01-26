83ca289c5c6f92bbf686ead242ac1eb1d8b5ed28
aQua <6028869+aQuaYi@users.noreply.github.com>
2020-01-26 12:33:51 +0800

    决定改变测试思路

--- context.go
+++ context.go
@@ -5,28 +5,36 @@ import (
 	"time"
 )
 
+// contextSim 实现了 context.Context 接口
 type contextSim struct {
+	// 所有与时间无关的方法，直接由此属性组合
 	context.Context
+	// 与时间相关的方法，通过以下属性改写
 	deadline time.Time
 	done     chan struct{}
 	err      error
 }
 
-func (s *Simulator) newContextSim(child context.Context, deadline time.Time) context.Context {
+func (s *Simulator) newContextSim(parent context.Context, deadline time.Time) context.Context {
 	ctx := &contextSim{
-		Context:  child,
+		Context:  parent,
 		done:     make(chan struct{}),
 		deadline: deadline,
 	}
 	t := s.newTimerFunc(deadline, nil)
 	go func() {
+		// 监控上下文的改变
 		select {
+		// 本上下文的时间到期了
 		case <-t.C:
 			ctx.err = context.DeadlineExceeded
-		case <-child.Done():
-			ctx.err = child.Err()
+		// 父上下文改变了
+		case <-parent.Done():
+			ctx.err = parent.Err()
+			// 停止本上下文的时间监控
 			defer t.Stop()
 		}
+		// 通知子上下文
 		close(ctx.done)
 	}()
 	return ctx
@@ -64,57 +72,57 @@ func Get(ctx context.Context) Clock {
 	return NewRealClock()
 }
 
-// After is a convenience wrapper for FromContext(ctx).After.
+// After is a convenience wrapper for Get(ctx).After.
 func After(ctx context.Context, d time.Duration) <-chan time.Time {
 	return Get(ctx).After(d)
 }
 
-// AfterFunc is a convenience wrapper for FromContext(ctx).AfterFunc.
+// AfterFunc is a convenience wrapper for Get(ctx).AfterFunc.
 func AfterFunc(ctx context.Context, d time.Duration, f func()) *Timer {
 	return Get(ctx).AfterFunc(d, f)
 }
 
-// NewTicker is a convenience wrapper for FromContext(ctx).NewTicker.
+// NewTicker is a convenience wrapper for Get(ctx).NewTicker.
 func NewTicker(ctx context.Context, d time.Duration) *Ticker {
 	return Get(ctx).NewTicker(d)
 }
 
-// NewTimer is a convenience wrapper for FromContext(ctx).NewTimer.
+// NewTimer is a convenience wrapper for Get(ctx).NewTimer.
 func NewTimer(ctx context.Context, d time.Duration) *Timer {
 	return Get(ctx).NewTimer(d)
 }
 
-// Now is a convenience wrapper for FromContext(ctx).Now.
+// Now is a convenience wrapper for Get(ctx).Now.
 func Now(ctx context.Context) time.Time {
 	return Get(ctx).Now()
 }
 
-// Since is a convenience wrapper for FromContext(ctx).Since.
+// Since is a convenience wrapper for Get(ctx).Since.
 func Since(ctx context.Context, t time.Time) time.Duration {
 	return Get(ctx).Since(t)
 }
 
-// Sleep is a convenience wrapper for FromContext(ctx).Sleep.
+// Sleep is a convenience wrapper for Get(ctx).Sleep.
 func Sleep(ctx context.Context, d time.Duration) {
 	Get(ctx).Sleep(d)
 }
 
-// Tick is a convenience wrapper for FromContext(ctx).Tick.
+// Tick is a convenience wrapper for Get(ctx).Tick.
 func Tick(ctx context.Context, d time.Duration) <-chan time.Time {
 	return Get(ctx).Tick(d)
 }
 
-// Until is a convenience wrapper for FromContext(ctx).Until.
+// Until is a convenience wrapper for Get(ctx).Until.
 func Until(ctx context.Context, t time.Time) time.Duration {
 	return Get(ctx).Until(t)
 }
 
-// ContextWithDeadline is a convenience wrapper for FromContext(ctx).ContextWithDeadline.
+// ContextWithDeadline is a convenience wrapper for Get(ctx).ContextWithDeadline.
 func ContextWithDeadline(ctx context.Context, d time.Time) (context.Context, context.CancelFunc) {
 	return Get(ctx).ContextWithDeadline(ctx, d)
 }
 
-// ContextWithTimeout is a convenience wrapper for FromContext(ctx).ContextWithTimeout.
+// ContextWithTimeout is a convenience wrapper for Get(ctx).ContextWithTimeout.
 func ContextWithTimeout(ctx context.Context, timeout time.Duration) (context.Context, context.CancelFunc) {
 	return Get(ctx).ContextWithTimeout(ctx, timeout)
 }
--- context_test.go
+++ context_test.go
@@ -1,1 +1,174 @@
 package clock
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func Test_contextSim(t *testing.T) {
+	Convey("利用 Simulator 新建 Context", t, func() {
+		now := time.Now()
+		s := NewSimulator(now)
+		parent, cancel := context.WithCancel(context.Background())
+		deadline := now.Add(time.Second)
+		cs := s.newContextSim(parent, deadline)
+		Convey("子文已经具备了 deadline", func() {
+			actual, ok := cs.Deadline()
+			So(actual, ShouldEqual, deadline)
+			So(ok, ShouldBeTrue)
+		})
+		Convey("新子文的还不会自己改变", func() {
+			err := cs.Err()
+			So(err, ShouldBeNil)
+		})
+		Convey("父文取消的话，子文也会结束", func() {
+			cancel()
+			<-cs.Done()
+			So(s.Now().Before(deadline), ShouldBeTrue)
+			err := cs.Err()
+			So(err, ShouldEqual, context.Canceled)
+		})
+		Convey("子文到期的话，也会结束", func() {
+			s.SetOrPanic(deadline)
+			<-cs.Done()
+			err := cs.Err()
+			So(err.Error(), ShouldEqual, context.DeadlineExceeded.Error())
+		})
+	})
+}
+
+func Test_Set_and_Get(t *testing.T) {
+	Convey("测试 Set 和 Get", t, func() {
+		now := time.Now()
+		s := NewSimulator(now)
+		parent := context.Background()
+		Convey("parent 中取出 realClock", func() {
+			_, ok := Get(parent).(realClock)
+			So(ok, ShouldBeTrue)
+		})
+		child := Set(parent, s)
+		Convey("child 中可以取出 s", func() {
+			actual, ok := Get(child).(*Simulator)
+			So(ok, ShouldBeTrue)
+			So(actual, ShouldEqual, s)
+		})
+	})
+}
+
+func Test_After(t *testing.T) {
+	Convey("测试 After", t, func() {
+		ctx := context.Background()
+		Convey("返回值的类型应该符合预期", func() {
+			actual := After(ctx, time.Second)
+			expected := make(<-chan time.Time)
+			So(actual, ShouldHaveSameTypeAs, expected)
+		})
+	})
+}
+
+func Test_AfterFunc(t *testing.T) {
+	Convey("测试 AfterFunc", t, func() {
+		ctx := context.Background()
+		Convey("返回值的类型应该符合预期", func() {
+			actual := AfterFunc(ctx, time.Second, func() {})
+			expected := &Timer{}
+			So(actual, ShouldHaveSameTypeAs, expected)
+		})
+	})
+}
+
+func Test_NewTicker(t *testing.T) {
+	Convey("测试 NewTicker", t, func() {
+		ctx := context.Background()
+		Convey("返回值的类型应该符合预期", func() {
+			actual := NewTicker(ctx, time.Second)
+			expected := &Ticker{}
+			So(actual, ShouldHaveSameTypeAs, expected)
+		})
+	})
+}
+
+func Test_NewTimer(t *testing.T) {
+	Convey("测试 NewTimer", t, func() {
+		ctx := context.Background()
+		Convey("返回值的类型应该符合预期", func() {
+			actual := NewTimer(ctx, time.Second)
+			expected := &Timer{}
+			So(actual, ShouldHaveSameTypeAs, expected)
+		})
+	})
+}
+
+func Test_Now(t *testing.T) {
+	Convey("测试 Now", t, func() {
+		ctx := context.Background()
+		Convey("返回值的类型应该符合预期", func() {
+			actual := Now(ctx)
+			expected := time.Now()
+			So(actual, ShouldHaveSameTypeAs, expected)
+		})
+	})
+}
+
+func Test_Since(t *testing.T) {
+	Convey("测试 Since", t, func() {
+		ctx := context.Background()
+		Convey("返回值的类型应该符合预期", func() {
+			actual := Since(ctx, time.Now())
+			expected := time.Second
+			So(actual, ShouldHaveSameTypeAs, expected)
+		})
+	})
+}
+
+func Test_Sleep(t *testing.T) {
+	Convey("测试 Sleep", t, func() {
+		ctx := context.Background()
+		Convey("返回值的类型应该符合预期", func() {
+			dur := time.Millisecond * 10
+			delta := time.Millisecond
+			start := time.Now()
+			Sleep(ctx, dur)
+			end := time.Now()
+			So(end, ShouldHappenWithin, dur+delta, start)
+		})
+	})
+}
+
+func Test_Tick(t *testing.T) {
+	Convey("测试 Tick", t, func() {
+		ctx := context.Background()
+		Convey("返回值的类型应该符合预期", func() {
+			actual := Tick(ctx, time.Second)
+			expected := make(<-chan time.Time)
+			So(actual, ShouldHaveSameTypeAs, expected)
+		})
+	})
+}
+
+func Test_Until(t *testing.T) {
+	Convey("测试 Until", t, func() {
+		ctx := context.Background()
+		Convey("返回值的类型应该符合预期", func() {
+			actual := Until(ctx, time.Now())
+			expected := time.Second
+			So(actual, ShouldHaveSameTypeAs, expected)
+		})
+	})
+}
+
+func Test_ContextWithDeadline(t *testing.T) {
+	Convey("测试 ContextWithDeadline", t, func() {
+		ctx := context.Background()
+		Convey("返回值的类型应该符合预期", func() {
+			ctx, cancel := ContextWithDeadline(ctx, time.Now().Add(time.Second))
+			var expectCtx context.Context
+			So(ctx, ShouldHaveSameTypeAs, expectCtx)
+			var expectCancel context.CancelFunc
+			So(cancel, ShouldHaveSameTypeAs, expectCancel)
+		})
+	})
+}
