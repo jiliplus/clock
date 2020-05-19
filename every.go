dc43bbc4ba6d37ea97d33710c98dec41455ab34f
aQuaYi.com <6028869+aQuaYi@users.noreply.github.com>
2020-05-19 09:51:11 +0800

    change nextTime to nextDayTime

--- every.go
+++ every.go
@@ -2,10 +2,10 @@ package clock
 
 import "time"
 
-// nextTime 是为了 EveryDay 函数准备的
+// nextDayTime 是为了 EveryDay 函数准备的
 // 是为了每天在 hour:minute:second 提供 tick
 // NOTICE: hour 是 24 小时制的小时
-func nextTime(now time.Time, hour, minute, second int) time.Time {
+func nextDayTime(now time.Time, hour, minute, second int) time.Time {
 	yyyy, mm, dd := now.Year(), now.Month(), now.Day()
 	loc := time.Now().Location()
 	next := time.Date(yyyy, mm, dd, hour, minute, second, 0, loc)
--- every_test.go
+++ every_test.go
@@ -7,29 +7,29 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func Test_nextTime(t *testing.T) {
-	Convey("测试 nextTime 函数", t, func() {
+func Test_nextDayTime(t *testing.T) {
+	Convey("测试 nextDayTime 函数", t, func() {
 		yyyy, mm, dd := 2020, time.Month(5), 18
 		h, m, s, ns := 21, 40, 34, 0
 		Convey("设置时间晚于 now 的时间段", func() {
 			loc := time.Now().Location()
 			now := time.Date(yyyy, mm, dd, h, m, s, ns, loc)
-			m++
-			expected := now.Add(time.Minute)
 			So(m, ShouldBeLessThan, 60)
 			Convey("则下一个时间点在当天", func() {
-				actual := nextTime(now, h, m, s)
+				actual := nextDayTime(now, h, m, s)
+				m++
+				expected := now.Add(time.Minute)
 				So(actual, ShouldEqual, expected)
 			})
 		})
 		Convey("设置时间早于 now 的时间段", func() {
 			loc := time.Now().Location()
 			now := time.Date(yyyy, mm, dd, h, m, s, ns, loc)
-			m--
-			expected := now.Add(-time.Minute).Add(24 * time.Hour)
 			So(m, ShouldBeGreaterThanOrEqualTo, 0)
 			Convey("则下一个时间点在下一天", func() {
-				actual := nextTime(now, h, m, s)
+				actual := nextDayTime(now, h, m, s)
+				m--
+				expected := now.Add(-time.Minute).Add(24 * time.Hour)
 				So(actual, ShouldEqual, expected)
 			})
 		})
@@ -37,9 +37,9 @@ func Test_nextTime(t *testing.T) {
 			loc := time.Now().Location()
 			ns = 0
 			now := time.Date(yyyy, mm, dd, h, m, s, ns, loc)
-			expected := now.Add(24 * time.Hour)
 			Convey("则下一个时间点在下一天", func() {
-				actual := nextTime(now, h, m, s)
+				actual := nextDayTime(now, h, m, s)
+				expected := now.Add(24 * time.Hour)
 				So(actual, ShouldEqual, expected)
 			})
 		})
